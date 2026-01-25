03ed1db7c8ab8ea0b13ee43cc5d499ff3097b007
Vlad Manea <vladmanea@Vlads-MacBook-Air.local>
2026-01-25 17:36:44 +0000

    chore(next): add redirect from /analyze to /analyse

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/analyze',
+        destination: '/analyse',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
